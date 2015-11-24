719cafe0f7e9614e0eae5510e88bda62add6df6c
jdkoen <koen.joshua@gmail.com>
2015-11-24 15:40:34 -0600

    added/updated file

--- probability_distributions/vonmises_mix_unif_pdf.m
+++ probability_distributions/vonmises_mix_unif_pdf.m
@@ -1,9 +1,9 @@
 % USAGE:
-%   p = wrapped_cauchy_mix_unif_pdf(x,mu,scale,lambda,LL,UL)
+%   p = vonmises_mix_unif_pdf(x,mu,sigma,lambda,LL,UL)
 %
 % DESCRIPTION:
-%   Returns the probability density at each x from a mixture of a wrapped
-%   Cauchy distribution and a continuous uniform distribution. The input
+%   Returns the probability density at each x from a mixture of a von Mises
+%   distribution and a continuous uniform distribution. The input
 %   parameters are mu (peak position of distribution), the scaling factor,
 %   the mixing parameter (lambda), and the lower (LL) and upper (UL)
 %   limits of the uniform distribution. 
@@ -12,21 +12,22 @@
 %   x       - Vector of observed data point(s). All values must be between
 %             pi and -pi.
 %   mu      - (Scaler) Mean of the (unwrapped) distribution.
-%   scale   - (Scalar) Scaling factor.
+%   sigma   - (Scalar) Standard devision of the distribution. In the
+%             formula, convert to the scale factor (1/sigma)
 %   lambda  - (Scalar) Mixing rate of the wrapped Cauchy (lambda) and the
 %             uniform (1-lambda) distributions. 
 %   LL      - (Scalar) Lower limit of the uniform distribution.
 %   UL      - (Scalar) Upper limit of the uniform distribution.
 %
 % SEE ALSO:
-%   UNIFPDF.m WRAPPED_CAUCHYPDF.m
+%   UNIFPDF.m VONMISESPDF.m
 %
 % Created by: Joshua D. Koen
 % Created on: 11/18/2015
 
-function p = wrapped_cauchy_mix_unif_pdf(x,mu,scale,lambda,LL,UL)
+function p = vonmises_mix_unif_pdf(x,mu,sigma,lambda,LL,UL)
 
-p = ( lambda * wrapped_cauchypdf(x,mu,scale) ) + ...
+p = ( lambda * vonmisespdf(x,mu,sigma) ) + ...
     ( ( 1 - lambda ) * unifpdf(x,LL,UL) );
 
 end
